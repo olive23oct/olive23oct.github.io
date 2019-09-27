d7b8a4a27b7f0fe3ee7c3df0f91f42604b185b65
olive23oct <olive23oct@gmail.com>
2019-09-27 20:24:36 +0900

    portfolio

---
+++ pages/profile.js
@@ -0,0 +1,17 @@
+import Nav from '../components/nav'
+import Head from 'next/head'
+import Profile from '../components/aboutMe';
+export default () => {
+    return(
+        <>
+        <Head>
+        <title>유희진 portfolio</title>
+        </Head>
+        <Nav/>
+
+        
+        
+        <Profile/>
+        </>
+    )
+};
---
+++ pages/project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head'
+import Nav from '../components/nav';
+import Works from '../components/works';
+
+export default () => {
+    return(
+        <>
+        <Nav/>
+        <Works/>
+        </>
+    )
+};
